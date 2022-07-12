d12e72e67388fae7cab28828a6446d47c617c1ae
codenameStarmunch <cold_storage@icloud.com>
2022-07-12 11:04:00 +0100

    Refactor tests to use 'each'

--- getLargest.test.js
+++ getLargest.test.js
@@ -1,4 +1,5 @@
 const getLargest = require("./getLargest");
+const each = require("jest-each");
 
 describe("getLargest", () => {
 
@@ -10,9 +11,17 @@ describe("getLargest", () => {
     test("getLargest is a function", () => {
         expect(typeof getLargest).toBe("function");
     });
-
+    
+    describe("It handles normal inputs successfully", () => {
+        
+        each([
+            [[1, 2, 3], 3],
+            [[9, 8, 7], 9],
+            [[12, 36, 17], 36]
+        ]).test("", (arr, expected) => {
+            expected(getLargest(arr).toEqual(expected))
+        })
+    })
 })
-
-
 
 
